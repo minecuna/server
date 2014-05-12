573c7f1db77bef1ff9e8bf5490779984ad30a239
George Hickman <george@ghickman.co.uk>
2014-05-12 08:05:52 +0100

    Use the overviewer config

--- overviewer.sh
+++ overviewer.sh
@@ -4,10 +4,11 @@
 config="$HOME/config/overviewer-config"
 logs="/var/log/minecraft/overviewer.log"
 overviewer="/usr/bin/overviewer.py"
+s3cfg="$HOME/.s3cfg-overviewer"
 s3cmd="/usr/bin/s3cmd"
 
 $overviewer --config=$config >> $logs
 
 # Push to s3 and invalidate the cloudfront distro
 
-$s3cmd --cf-invalidate sync --delete-removed --no-progress $HOME/overviewer/* s3://minecuna-overviewer >> $logs
+$s3cmd --config=$s3cfg --cf-invalidate sync --delete-removed --no-progress $HOME/overviewer/* s3://minecuna-overviewer >> $logs
