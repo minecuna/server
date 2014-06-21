a9fb486323daef9d3455d6e0437f10837d88c5e0
George Hickman <george@ghickman.co.uk>
2014-06-22 00:40:48 +0100

    Generate PoIs

--- overviewer.sh
+++ overviewer.sh
@@ -8,6 +8,7 @@ s3cfg="$HOME/.s3cfg-overviewer"
 s3cmd="/usr/bin/s3cmd"
 
 $overviewer --config=$config >> $logs 2>&1
+$overviewer --config=$config --genpoi >> $logs 2>&1
 
 # Push to s3 and invalidate the cloudfront distro
 
