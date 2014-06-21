14854da81f5689a7b7ef8eb3d955a46460d7d06a
George Hickman <george@ghickman.co.uk>
2014-06-22 00:39:58 +0100

    Redirect stderr to stdout for overviewer logs

--- overviewer.sh
+++ overviewer.sh
@@ -7,7 +7,7 @@ overviewer="/usr/bin/overviewer.py"
 s3cfg="$HOME/.s3cfg-overviewer"
 s3cmd="/usr/bin/s3cmd"
 
-$overviewer --config=$config >> $logs
+$overviewer --config=$config >> $logs 2>&1
 
 # Push to s3 and invalidate the cloudfront distro
 
